14c2a1cbb12afbea631c49b112ede9c3998945bf
yunhwan <sky10240107@naver.com>
2022-03-21 20:33:58 +0900

    props 사용

--- src/App.js
+++ src/App.js
@@ -1,34 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
 
-function Header(){//컴포넌트 생성(대문자로 시작)
-  return <header>
-  <h1><a href="/">WEB</a></h1>
+function Header(props){//컴포넌트 생성(대문자로 시작)
+  return <header> 
+  <h1><a href="/">{props.title}</a></h1> 
 </header>
 
 }
-function Nav(){
+function Nav(props){
+  const lis =[]
+  for(let i=0; i<props.topics.length; i++){ //topics의 개수만큼 반복
+      let t = props.topics[i]; 
+      lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>)  //lis에 하나씩 넣기, 동적으로 바뀔대상이 있으면 중괄호 사용
+  }
   return <nav>
     <ol>
-      <li><a href="/read/1">html</a></li>
-      <li><a href="/read/2">css</a></li>
-      <li><a href="/read/3">js</a></li>
+      {lis}
     </ol>
   </nav>
 }
-function Article(){
+function Article(props){//props를 사용시 함수 외부에서 입력값으로 사용가능
   return <article>
-    <h2>Welcome</h2>
-    Hello, WEB
+    <h2>{props.title}</h2>
+    {props.body}
   </article>
 }
 
 function App() {
+  const topics = [//topics를 배열 설정
+    {id:1, title:'html', body:'html is ...'},
+    {id:2, title:'css', body:'css is ...'},
+    {id:3, title:'javascript', body:'javascript is ...'},
+  ]
   return (
     <div>
-      <Header></Header>
-      <Nav></Nav>
-      <Article></Article>
+      <Header title="WEB"></Header>
+      <Nav topics={topics}></Nav>
+      <Article title="Welcome" body="Hello, WEB"></Article>
       
     </div>
   );
