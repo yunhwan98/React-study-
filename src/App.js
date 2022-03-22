681015ce43f2295ae6290884f2cf3e85ec2783e4
yunhwan <sky10240107@naver.com>
2022-03-22 18:03:31 +0900

    update 기능 추가

--- src/App.js
+++ src/App.js
@@ -55,6 +55,32 @@ function Create(props){  //Create 컴포넌트 생성
 
 }
 
+function Update(props){
+  const [title, setTitle] =useState(props.title); //props에 들어온 title에서 state로
+  const [body, setBody] =useState(props.body);
+  return <article>
+    <h2>Update</h2>
+    <form onSubmit={event=>{
+      event.preventDefault();
+      const title = event.target.title.value; //event target은 form 태그 지칭
+      const body = event.target.body.value;
+      props.onUpdate(title,body);
+    }}>
+      <p><input type="text" name="title" placeholder="title" value={title} onChange={event=>{//state를 value값으로 줌
+          setTitle(event.target.value); //새로 입력할때 마다 setTitle지정, 컴포넌트 다시 실행
+
+      }}/></p>
+      <p><textarea name="body" placeholder="body" value={body} onChange={event=>{//state를 value값으로 줌
+          setBody(event.target.value); //새로 입력할때 마다 setTitle지정, 컴포넌트 다시 실행
+
+      }}></textarea></p>
+      <p><input type="submit" value="Update"></input></p> 
+    </form>
+  </article>
+  //기존 내용이 저장되게 value값을 설정해줌
+  //react에서는 값을 입력할 때마다 onChange 발생
+
+}
 
 function App() {
   //const _mode = useState('WELCOME');  //mode의 값에 따라 본문 변화
@@ -69,6 +95,7 @@ function App() {
     {id:3, title:'javascript', body:'javascript is ...'},
   ]);
   let content = null;
+  let contextControl = null;
   if(mode === 'WELCOME'){ //mode의 따라 결정
       content = <Article title="Welcome" body="Hello, WEB"></Article>
   }else if(mode === 'READ'){
@@ -81,7 +108,10 @@ function App() {
         }
       }
       content = <Article title={title} body={body}></Article>
-
+      contextControl = <li><a href ={'/update/'+id} onClick={event=>{
+          event.preventDefault();
+          setMode('UPDATE');  //mode를 UPDATE로 변경
+      }}>Update</a></li> //mode가 READ일 때만 나타나기
   }else if(mode === 'CREATE'){
       content = <Create onCreate={(_title, _body)=>{  //Create버튼 눌렀을 때 실행함수
       const newTopic = {id:nextId,title:_title, body:_body} 
@@ -92,7 +122,30 @@ function App() {
       setId(nextId);  
       setNextId(nextId+1);  //다음에 글을 추가할 때 대비
       }}></Create>
+  }else if(mode === 'UPDATE'){
+    //title과 body 알아내기 위해 앞의 코드 복사
+    let title, body = null;
+      for(let i=0; i<topics.length; i++){ //topics.id와 id state가 일치하면 title,body 값 설정
+        console.log(topics[i].id, id);
+        if(topics[i].id === id){
+            title = topics[i].title;
+            body = topics[i].body;
+        }
+      }
+    content = <Update title={title} body={body} onUpdate={(title,body)=>{
+        console.log(title,body);
+        const newTopics = [...topics]  //기존 토픽 복제
+        const updatedTopic = {id:id, title:title, body:body}  //수정할 토픽 생성
+        for(let i=0; i<newTopics.length; i++){  //기존토픽과 일치하는 id찾기
+          if(newTopics[i].id === id){
+            newTopics[i] = updatedTopic;
+            break;
+          }
 
+        }
+        setTopics(newTopics); //토픽이 바뀌면 컴포넌트 새로 실행
+        setMode('READ');
+    }}></Update>
   }
   return (
     <div>
@@ -104,10 +157,14 @@ function App() {
         setId(_id);   //_id값이 바뀌면 컴포넌트가 새로 실행되며 새로운 id값 지정 해줌
       }}></Nav>
       {content}
-      <a href = "/create" onClick={event=>{
-        event.preventDefault(); //url 바뀌는 것 방지
-        setMode('CREATE');
-      }}>Create</a>
+      <ul>
+        <li><a href = "/create" onClick={event=>{
+          event.preventDefault(); //url 바뀌는 것 방지
+          setMode('CREATE');
+        }}>Create</a></li>
+        {contextControl}
+      </ul>
+      
     </div>
   );
 }
